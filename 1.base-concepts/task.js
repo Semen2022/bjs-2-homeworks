636fc610badebafff81e19cce293271832056ef2
Semen2022 <study2022@mail.ru>
2023-02-05 16:46:39 +0300

    Создана функция solveEquation()

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+
+  if (a === 0) {
+    return 'Уравнение не является квадратным';
+  } else {
+    let discriminant = b ** 2 - 4 * a * c;
+
+    if (discriminant === 0) {
+      arr = [- b / (2 * a)];
+    } else if (discriminant > 0) {
+      arr = [(- b - Math.sqrt(discriminant)) / (2 * a),
+       (- b + Math.sqrt(discriminant)) / (2 * a),
+      ]
+    }
+  }
   
   return arr;
 }
