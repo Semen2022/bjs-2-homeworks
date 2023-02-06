5f8cd4d4a917be2c7cb88ec0b06f84e8e7bdf207
Semen2022 <study2022@mail.ru>
2023-02-06 21:47:03 +0300

    Проверка на "число" во входн. парам. - выполняется

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
 
@@ -23,5 +23,26 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  // т.к. в начале уже установлено "use ctrict"; повторной активации строго режима не требуется
+  let percentIn = parseFloat(percent); // преобрузуем переменные в числа
+  let contributionIn = parseFloat(contribution);
+  let amountIn = parseFloat(amount);
+  let countMonthsIn = parseInt(countMonths);
+
+  if (Number.isNaN(percentIn)) {
+    return console.log('Параметр <процентная ставка> содержит неправильное значение ' + '<' + percent + '>');
+  }
+
+  if (Number.isNaN(contributionIn)) {
+    console.log(`Параметр <первоначальный взнос> содержит неправильное значение <${contribution}>`);
+    return; 
+  }
+
+  if (Number.isNaN(amountIn)) {
+    return console.log(`Параметр <amount> содержит неправильное значение <${amount}>`);
+  } // пробовал разными способами пройти тест, вернув в нескольких "видах" строку, - не получается.
+
+
+
+
+} 
