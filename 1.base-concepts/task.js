702c27446d3507a30f07dc3051870fd2783d7bce
Semen2022 <study2022@mail.ru>
2023-02-07 18:48:24 +0300

    Правки по условию корректности данных

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -25,24 +25,19 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   // т.к. в начале уже установлено "use ctrict"; повторной активации строго режима не требуется
   let percentIn = parseFloat(percent); // преобрузуем переменные в числа
-  let contributionIn = parseFloat(contribution); // предположил, что числа могут 
+  let contributionIn = parseFloat(contribution); // предположим, что числа могут 
   let amountIn = parseFloat(amount);             // быть в общем случае - дробные.
   let countMonthsIn = parseInt(countMonths); // количество месяцев обычно число целое
 
       // ниже идут проверки на вводимые даные - должно быть число.
   if (Number.isNaN(percentIn)) {
     return console.log('Параметр \"Процентная ставка\" содержит неправильное значение ' + '<' + percent + '>');
-  }
-
-  if (Number.isNaN(contributionIn)) {
+  } else if (Number.isNaN(contributionIn)) {
     console.log(`Параметр \"Начальный взнос\" содержит неправильное значение <${contribution}>`);
     return; 
-  }
-
-  if (Number.isNaN(amountIn)) {
-    return console.log(`Параметр \"Общая стимость\" содержит неправильное значение <${amount}>`);
-  } // пробовал разными способами пройти тест, вернув в нескольких "видах" строку, - не получается.
-
+  } else if (Number.isNaN(amountIn)) {
+    return `Параметр \"Общая стимость\" содержит неправильное значение <${amount}>`;
+  } else { // пробовал разными способами пройти тест, вернув в нескольких "видах" строку, - не получается.
   let creditBody = amountIn - contributionIn; // вычисление тела кредита
   let percentMonth = percentIn / 12 / 100 ;
   let everyMonthPay = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonthsIn) - 1)));
@@ -53,4 +48,5 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   console.log(allAmount);
 
   return allAmount;
+  }
 } 
