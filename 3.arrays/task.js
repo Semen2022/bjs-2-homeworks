b93e94c1b6332b3f0798bbe0092203f3e78fd677
Semen2022 <study2022@mail.ru>
2023-03-05 21:20:23 +0300

    Home work 3.Array - ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+    /*устанавливаем два условия, которые должны выполняться вместе, т.е. "логическое И "*/
+    /** первое условие - сравниваем массивы по длине */
+    /** второе условие - используя функцию высшего порядка - метод .every()
+     * задаем в аргументе callback-функцию , в которую передаем элемент и его индекс.
+     * Для каждого элемента первого массива сравниваем элемент второго массива 
+     * на этой же позиции*/ 
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if ((users.length === 0 && gender === "мужской") || (gender !== "мужской" && gender !== "женский")) {
+        return 0;
+    }
+
+    let result = users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, idx, arr) => {
+        acc += item;
+        if (idx === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0);
+    return result;
 }
