e2a1fd5a712de0f73e65f1a11cac58ee81080986
Semen2022 <study2022@mail.ru>
2023-03-18 22:45:49 +0300

    Исправление1 Чертвертого задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,20 +15,62 @@ Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
+// console.log(peter);
+
 Student.prototype.addMarks = function (...marks) {
-    this.marks?.push(...marks); // правильно применен оператор опциональной последовательности?
+    //console.log("Хочу добавить оценки.");
+    if (this.marks) {  //console.log("Добавляю оценки.", marks);
+        this.marks.push(...marks);
+    } /*else {
+        return console.log("Добавление оценок невозможно, пользователь (студент) отчислен.");
+    }*/
 }
 
+/*
+ //Метод "Student.prototype.addMarks" реализован через "цепочку логических "И" "
+ Student.prototype.addMarks = function (...marks) {
+    this.marks && this.marks.push(...marks); 
+}*/
+
+
+/*  //Метод "Student.prototype.addMarks" реализован через оператор опциональной последовательности
+ Student.prototype.addMarks = function (...marks) {
+    this.marks?.push(...marks); // правильно ли применен оператор опциональной последовательности? Ответ преподавателя - правильно.
+}
+*/
+
 Student.prototype.getAverage = function () {
-    // console.log("Массив оценок - ", this.marks)
-    if (!this.marks) {
+    if (!this.marks || this.marks.length === 0) { // А как еще можно описать проверку на наличие массива?
         return 0;
-    } else {
-        // console.log('Среднее - ', this.marks.reduce((accum, item) => accum + item) / this.marks.length, this.marks.length);
-        // console.log('АККУМ - ', this.marks.reduce((accum, item) => accum + item));
-    return this.marks.reduce((accum, item) => accum + item) / this.marks.length;
+    } else {        /** закомментированны код ниже две попытки поиска решения. Надо правильно установить условие:
+                        1) отсутствие (неналичие) массива оценок = true ИЛИ 2) длина массива равна нулю = true , тогда вернуть 0 */
+                    //console.log('Среднее - ', this.marks.reduce((accum, item) => accum + item, 0) / this.marks.length, this.marks.length);
+                    //console.log('АККУМ - ', this.marks.reduce((accum, item) => accum + item));
+                //   let average = this.marks.reduce((accum, item) => accum + item) / this.marks.length;
+                    //console.log(average, " - среднее отдельно")
+                //   return average;  }} // 
+            // код ниже - функционал поиска среднего арифметического с делением на длину массива внутри коллбэка. 
+            // Хотя мне более изящным кажется решение 01 на 65 строке. 
+        let averageMark = this.marks.reduce((acc, item, idx, arr) => {
+            acc += item;
+            if (idx === arr.length - 1) {
+                return acc / arr.length;
+            }
+            return acc
+        }, 0);
+        return averageMark;
+    }
+} 
+
+/* // Решение 01
+Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    } else {  //если длина массива marks будет нулевой, ход программы сюда не попадет.
+        return this.marks.reduce((accum, item) => accum + item) / this.marks.length; 
     }
 }
+*/
 
 Student.prototype.exclude = function (reason) {
     delete this.subject;
