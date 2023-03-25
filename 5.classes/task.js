adee745f74786f70c30fd0fbe227292661ee4f62
Semen2022 <study2022@mail.ru>
2023-03-26 00:36:25 +0300

    Пятое задание решено

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,5 +1,5 @@
 'use strict'
-
+            /** Решение задачи №1. Печатное издание*/
 class PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
         this.name = name;
@@ -69,4 +69,45 @@ class DetectiveBook extends Book {
     }
 }
 
+/** Решение задачи №2. Библиотека */
 
+class Library {
+    constructor(name, books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30){
+            this.books.push(book);
+        }
+    }
+
+        // Первый вариант решения поиска книги.
+/*    findBookBy(type, value) {
+        //console.log(this.books, value);
+        let idx = this.books.findIndex((findBook) => findBook[type] === value);
+        if (idx !== -1) {
+            return this.books[idx];
+        } else {
+            return null;
+        }
+    }
+    */
+        // Второй вариант поиска книги
+    findBookBy(type, value) {
+        let findingBook = this.books.find(findBookItem => findBookItem[type] === value);
+        return findingBook || null; 
+    }
+
+    giveBookByName(bookName) {
+        let findBookIdx = this.books.findIndex(findBookItem => findBookItem.name === bookName);
+        const readerFindBook = this.books[findBookIdx];
+        if (findBookIdx !== -1) {
+            this.books.splice(findBookIdx, 1); // пробовал присваивать в переменную удаленный массив, но значение не возвращалось...
+            return  readerFindBook;
+        } else {
+            return null;
+        }
+    }
+}
