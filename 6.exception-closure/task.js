4551fdf02542cc71285c22897a51acd1d1a98910
Semen2022 <study2022@mail.ru>
2023-03-27 00:34:46 +0300

    Решена задача  №1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,27 @@
-﻿
+﻿'use strict'
+        // Решение задачи №1
+function parseCount(arg) {
+    let parsArg = null;
+
+    if (typeof arg === 'number') {
+        parsArg = arg;
+    }
+    if (typeof arg === 'string') {
+        parsArg = Number.parseFloat(arg);
+    }
+    if (Number.isNaN(parsArg)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsArg;
+}
+
+function validateCount(params) {
+    try{
+        return parseCount(params);
+    } catch(error) {
+        console.log(error);
+        return error;
+    }
+}
+
+        // Решение задачи №2
