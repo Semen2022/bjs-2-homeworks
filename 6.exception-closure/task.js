ffda62913840d5dddcfdc93236612b523d0a4f43
Semen2022 <study2022@mail.ru>
2023-03-27 21:36:48 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -25,3 +25,70 @@ function validateCount(params) {
 }
 
         // Решение задачи №2
+/* // Вариант первый
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        if (sideA > sideB + sideC || sideB > sideA + sideC || sideC > sideB + sideA) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+    }
+    
+    get perimeter() {
+        return Number((this.sideA + this.sideB + this.sideC).toFixed(3));
+    }
+    get area(){
+        let halfPerimetr = this.perimeter / 2;
+        return Number((Math.sqrt(halfPerimetr * (halfPerimetr - this.sideA) * (halfPerimetr - this.sideB) * (halfPerimetr - this.sideC))).toFixed(3));
+    }
+}
+*/
+
+function getTriangle(a, b, c) {
+    try {
+        let newTriangle = new Triangle(a, b, c);
+        return newTriangle;
+    } catch(error) {
+        const errorMessage = "Ошибка! Треугольник не существует";
+        return {
+            get area() {
+                return errorMessage;
+            },
+            get perimeter() {
+                return errorMessage;
+            }
+        }; // подскажите, в чем смысл этих двух get-теров еще и в return?
+    } 
+}
+
+/*/
+let trtr = new Triangle(300, 400, 500);
+console.log(trtr, trtr.perimeter, trtr.area);
+*/
+        // Вариант второй
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        let arrSidesTriangle = [sideA, sideB, sideC].sort((a, b) => a-b);
+        if (arrSidesTriangle[2] > arrSidesTriangle[1] + arrSidesTriangle[0]) {
+            console.log('мы не сможем построить такой треугольник');
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+        // console.log(arrSidesTriangle, arrSidesTriangle[2], arrSidesTriangle[1], arrSidesTriangle[0]);
+    }
+
+    get perimeter() {
+        return Number((this.sideA + this.sideB + this.sideC).toFixed(3));
+    }
+    get area(){
+        let halfPerimetr = this.perimeter / 2;
+        return Number((Math.sqrt(halfPerimetr * (halfPerimetr - this.sideA) * (halfPerimetr - this.sideB) * (halfPerimetr - this.sideC))).toFixed(3));
+    }
+}
+
