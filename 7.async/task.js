351232c9e55cc76b9b66defb4f3b32f04de49eb2
Semen2022 <study2022@mail.ru>
2023-04-04 22:48:55 +0300

    Добавлено время будильник в HH:mm

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,15 +3,15 @@
 class  AlarmClock {
     constructor() {
         this.alarmCollection = [];
-        this.inervalId = null; 
+        this.intervalId = null; 
     }
 
     addClock(timeStart, funcCallBack) {
-        if (timeStart == undefined && funcCallBack == undefined ) {
+        if (timeStart == undefined || funcCallBack == undefined ) {
             throw new Error("Отсутствуют обязательные аргументы");
         }
 
-        if (timeStart === this.inervalId) {
+        if (timeStart === this.intervalId) {
             console.warn("Уже присутствует звонок на это же время");
         }
 
@@ -28,5 +28,31 @@ class  AlarmClock {
         return new Date().toLocaleTimeString().slice(0,-3);
     }
 
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => this.alarmCollection.forEach(timeAlarm => {
+                if (timeAlarm.time === this.getCurrentFormattedTime() && timeAlarm.canCall) {
+                    timeAlarm.canCall = false;
+                    timeAlarm.callback();
+                }
+              }), 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    } 
+
+    resetAllCalls() {
+        this.alarmCollection.forEach( itemCanCall => itemCanCall.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
 }
 
