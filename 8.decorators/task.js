0810350984ff8f5575b45645ca91868ea636d5cc
Semen2022 <study2022@mail.ru>
2023-04-09 10:21:01 +0300

    Первая задача готова

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,21 +4,15 @@ function cachingDecoratorNew(func) {
 
     function wrapper(...args) {
         const hash = md5(args); // получаем правильный хеш c помощью функции md5
-        console.log(`-*-*-*- hash : ${hash} -*-*-*-`);
-        let index = null;
-        console.log(cache);
-        let objectInCache = cache.find((item) => {
-            console.log(`item ЭТО ${item}`);
-            console.log(`item.hash ЭТО ${item.hash}`);
-            return item.hash === hash;}); // ищем элемент, хеш которого равен нашему хешу
-                console.log(` objectInCache - ${objectInCache} -*-*-*-*-`);
+            // console.log(cache);
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
         if (objectInCache) { // если элемент найден
-            console.log("Из кэша: " + objectInCache.hash); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-            return "Из кэша: " + objectInCache.hash;
+            console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + objectInCache.value;
         }
 
         let result = func(...args); // в кеше результата нет — придётся считать
-        cache.push({hash: md5(args), value: result}) ; // добавляем элемент с правильной структурой
+        cache.push({hash: hash, value: result}) ; // добавляем элемент с правильной структурой
         if (cache.length > 5) { 
         cache.shift() // если слишком много элементов в кеше, надо удалить самый старый (первый) 
         }
